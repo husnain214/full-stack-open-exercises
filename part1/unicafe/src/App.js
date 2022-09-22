1f20027c71530248c5b30733da50115756ea5507
Husnain Zahid <husnainzahid214@gmail.com>
2022-09-22 22:44:49 +0500

    "commit"

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,51 +1,61 @@
 import { useState } from 'react'
 
-const Header = (props) => <h1>{props.text}</h1>
-const Button = (props) => <button onClick = {props.eventFunction}>{props.text}</button>
-const Paragraph = (props) => <p>{props.text} {props.count}</p> 
+const Heading = ({text}) => <h1>{text}</h1>
+
+const Buttons = ({goodInc, neutralInc, badInc}) => {
+  return (
+    <div>
+      <button onClick = {goodInc}>good</button>
+      <button onClick = {neutralInc}>neutral</button>
+      <button onClick = {badInc}>bad</button>
+    </div>
+  )
+}
+
+const StatisticLine = ({text, value, percent}) => {
+  return (
+    <>
+      <td>{text}</td>
+      <td>{value}</td>
+      <td>{percent}</td>
+    </>
+  )
+}
+
+const StatisticsTable = ({good, neutral, bad}) => {
+  const all = good + bad + neutral
+  const average = (good * 1 + bad * -1) / all
+  const positive = (good / all) * 100
+
+  if (all === 0) return "No feedback given"
+  
+  return (
+    <table>
+      <tr><StatisticLine text = "good" value = {good} percent = "" /></tr>
+      <tr><StatisticLine text = "neutral" value = {neutral} percent = "" /></tr>
+      <tr><StatisticLine text = "bad" value = {bad} percent = "" /></tr>
+      <tr><StatisticLine text = "all" value = {all} percent = "" /></tr>
+      <tr><StatisticLine text = "average" value = {average} percent = "" /></tr>
+      <tr><StatisticLine text = "positive" value = {positive} percent = "%" /></tr>
+    </table>
+  )
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
-  const [average, setAverage] = useState(0)
 
-  const goodInc = () => {
-    setGood(good + 1)
-    setTotal(total + 1)
-    console.log((good / total) * 100)
-  }
-
-  const neutralInc = () => {
-    setNeutral(neutral + 1)
-    setTotal(total + 1)
-    console.log((good / total) * 100)
-  }
-  
-  const badInc = () => {
-    setBad(bad + 1)
-    setTotal(total + 1)
-    console.log((good / total) * 100)
-  }
+  const goodInc = () => setGood(good + 1)
+  const neutralInc = () => setNeutral(neutral + 1)
+  const badInc = () => setBad(bad + 1)
 
   return (
     <div>
-      <Header text = "give feedback"/>
-
-      <div>
-          <Button text = "good" eventFunction = {goodInc} />
-          <Button text = "neutral" eventFunction = {neutralInc} />
-          <Button text = "bad" eventFunction = {badInc} />
-      </div>
-
-      <Header text = "statistics"/>
-
-      <Paragraph text = "good" count = {good} />
-      <Paragraph text = "neutral" count = {neutral} />
-      <Paragraph text = "bad" count = {bad} />
-      <Paragraph text = "all" count = {total} />
-      <Paragraph text = "average" count = {average} />
+      <Heading text="give feedback" />
+      <Buttons goodInc = {goodInc} neutralInc = {neutralInc} badInc = {badInc} />
+      <Heading text="statistics" />
+      <StatisticsTable good = {good} neutral = {neutral} bad = {bad} />
     </div>
   )
 }
